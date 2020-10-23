4eb84b542dc78235b782f9b161e5c00f13f43452
sneha <gapatsneha95@outlook.com>
2020-10-23 14:17:00 +0530

    update the controller file with http get,put,delete methods

--- NestSeeker.Web/Startup.cs
+++ NestSeeker.Web/Startup.cs
@@ -19,9 +19,12 @@ namespace NestSeeker.Web
 {
     public class Startup
     {
+        //private readonly IConfigurationRoot _configRoot;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            /* IConfigurationBuilder builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+           this._configRoot = builder.Build();*/
         }
 
         public IConfiguration Configuration { get; }
@@ -35,6 +38,7 @@ namespace NestSeeker.Web
 
             services.AddTransient<IUnitOfWork, UnitOfWork>(); //RP
             services.AddTransient<IRepository<User>, Repository<User>>(); //RP
+            // services.AddDbContext<NestSeekerContext>(Configuration, _configRoot);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
